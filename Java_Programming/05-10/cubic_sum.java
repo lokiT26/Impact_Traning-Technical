5291219be8f6d7da55a4042ac9ba478f7d9bdf2a
Lokesh <lokeshthakare2004@gmail.com>
2025-05-12 17:31:13 +0530

    Modified a file
    cubic_sum.java: added another possible solution for the problem

--- Java_Programming/05-10/cubic_sum.java
+++ Java_Programming/05-10/cubic_sum.java
@@ -7,59 +7,103 @@
 import java.util.Scanner;
 
 class cubic_sum {
-    // Function to check if a number can be expressed as sum of two cubes
-    private static boolean isGoodInteger(long num) {
-        // Find approximate cube root limit
-        int cubeRootLimit = (int) Math.cbrt(num) + 1;
-        
-        for (int x = -cubeRootLimit; x <= cubeRootLimit; x++) {
-            long x_cubed = (long) x * x * x;
-            
-            // Skip if x^3 > num (when x is positive)
-            if (x > 0 && x_cubed > num) continue;
-            
-            // Calculate what y^3 needs to be
-            long y_cubed_needed = num - x_cubed;
-            
-            // Check if y^3 has an exact cube root
-            double y_double = Math.cbrt(y_cubed_needed);
-            long y = Math.round(y_double);
-            
-            // Check if y^3 matches what we need (allowing for small floating point errors)
-            if (Math.abs(y * y * y - y_cubed_needed) < 1e-9) {
-                System.out.println(num+": "+x+" and "+y);
-                return true;
-            }
-        }
-        
-        return false;
-    }
-    
-    // Function to count good integers in array
-    public static int isCubicSumExist(long[] A, int N) {
-        int count = 0;
-        
-        for (int i = 0; i < N; i++) {
-            if (isGoodInteger(A[i])) {
-                count++;
-            }
-        }
-        
-        return count;
-    }
-    
+    // CODE 1: x, y and array's elements should be greater than 0.
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        long[] A = new long[N];
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        int count = 0;
 
-        for(int i = 0; i < N; i++){
-            A[i] = sc.nextLong();
+        for(int i = 0; i < n; i++){
+            arr[i] = sc.nextInt();
         }
-        
-        int result = isCubicSumExist(A, N);
-        System.out.println("Output: " + result);
 
+        for(int i = 0; i < n; i++){
+            int lb = 1;
+            int ub = (int)Math.cbrt(arr[i]);
+
+            while(lb<=ub){
+                int cube_lb = (int)Math.pow(lb, 3);
+                int cube_up = (int)Math.pow(ub, 3);
+                int z = cube_lb + cube_up;
+                if(z == arr[i]){
+                    System.out.println(arr[i]+": "+ lb+" and "+ub);
+                    count++;
+                    break;
+                }
+                else {
+                    if (z < arr[i]){
+                        lb++;
+                    }
+                    else {
+                        ub--;
+                    }
+                }
+                
+            }
+        }
+        System.out.println("Good Integers: "+ count);
         sc.close();
+
     }
+    
+
+    // CODE 2: x and y should be integers (can be positive and negative).
+
+    // Function to check if a number can be expressed as sum of two cubes
+    // private static boolean isGoodInteger(long num) {
+    //     // Find approximate cube root limit
+    //     int cubeRootLimit = (int) Math.cbrt(num) + 1;
+        
+    //     for (int x = -cubeRootLimit; x <= cubeRootLimit; x++) {
+    //         long x_cubed = (long) x * x * x;
+            
+    //         // Skip if x^3 > num (when x is positive)
+    //         if (x > 0 && x_cubed > num) continue;
+            
+    //         // Calculate what y^3 needs to be
+    //         long y_cubed_needed = num - x_cubed;
+            
+    //         // Check if y^3 has an exact cube root
+    //         double y_double = Math.cbrt(y_cubed_needed);
+    //         long y = Math.round(y_double);
+            
+    //         // Check if y^3 matches what we need (allowing for small floating point errors)
+    //         if (Math.abs(y * y * y - y_cubed_needed) < 1e-9) {
+    //             System.out.println(num+": "+x+" and "+y);
+    //             return true;
+    //         }
+    //     }
+        
+    //     return false;
+    // }
+    
+    // // Function to count good integers in array
+    // public static int isCubicSumExist(long[] A, int N) {
+    //     int count = 0;
+        
+    //     for (int i = 0; i < N; i++) {
+    //         if (isGoodInteger(A[i])) {
+    //             count++;
+    //         }
+    //     }
+        
+    //     return count;
+    // }
+    
+    // public static void main(String[] args) {
+    //     Scanner sc = new Scanner(System.in);
+    //     int N = sc.nextInt();
+    //     long[] A = new long[N];
+
+    //     for(int i = 0; i < N; i++){
+    //         A[i] = sc.nextLong();
+    //     }
+        
+    //     int result = isCubicSumExist(A, N);
+    //     System.out.println("Output: " + result);
+
+    //     sc.close();
+    // }
 }
